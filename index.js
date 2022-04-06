b4ddf83aaab16951f971afecdc785cc5dba86948
Alina <pisarenko.alina@gmail.com>
2022-04-06 09:04:34 -0400

    commit tree f2b500d563d5d13e032ad87cc1044217c8030654

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  return (bestCustomer = "maybe bob");
+}
+
+const leastFavoriteCustomer = "Daisy";
+function changeLeastFavoriteCustomer() {
+  return (leastFavoriteCustomer = "maybe bob");
+}
+changeLeastFavoriteCustomer();
